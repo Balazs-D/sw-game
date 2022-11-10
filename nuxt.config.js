fc9373ac11eb1345df80cfc32dd26e508e093aed
Balazs Danyadi <balazs.danyadi@gmail.com>
2022-11-10 11:24:17 +0100

    nuxt config prefix added

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: process.env.API_URL,
+    prefix: process.env.API_URL,
   },
 
   transpileDependencies: ['vuex-module-decorators'],
